423253ef9356894c5071585f9b447c6a20ad8c62
JT9 <caleb.tsosie@asu.edu>
2019-04-30 18:51:42 -0700

    Handling Form Submittal
    Prevent the entire page from refreshing when user presses enter on their keyboard to setup the search feature

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-    state = { term: 'dfafadfda'};
+    state = { term: ''};
 
-    onInputChange = () => {
-        
+    //Event Handler to handle user input to rerender content
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value });
+    };
+
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        //TODO: Make sure we call
+        //callback from parent component
     };
 
     render() {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
