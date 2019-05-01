b250c48700e66d804124cc6b14dba1d2027950df
JT9 <caleb.tsosie@asu.edu>
2019-04-30 21:16:21 -0700

    Nested Callbacks
    Using callbacks to communicate between parent and child in order to help update the components when user selects a video from the VideoList

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [], selectedVideo: null };
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
@@ -14,11 +14,17 @@ class App extends React.Component {
         
         this.setState({ videos: response.data.items });
     };
+
+    //callback to be used to communicate between parent and child
+    onVideoSelect = (video) => {
+        console.log('From the App!', video);
+    };
+
     render() {
         return ( 
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
